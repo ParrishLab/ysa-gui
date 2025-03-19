8f3319a71de558ddbb3c955ff1180fa32028fe16
Jacob Cahoon <booka66@Jacobs-MacBook-Pro-6.local>
2025-03-19 11:20:46 -0600

    updated test to inlclude hdf5 path

--- src/helpers/extensions/test.py
+++ src/helpers/extensions/test.py
@@ -1,4 +1,8 @@
+import os
+
 try:
+    hdf5_dir = "path/to/your/HDF5-1.14.5-win64"
+    os.environ["PATH"] = os.path.join(hdf5_dir, "bin") + os.pathsep + os.environ["PATH"]
     import sz_se_detect
 
     print("sz_se_detect module is installed")
