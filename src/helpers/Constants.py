1851a1cd540c37aebdba4ee7213b66e9e6ea47c9
Katelyn Chapman <kforbes@Katelyns-MacBook-Air-64.local>
2025-11-29 18:40:50 -0700

    Upping version number

--- src/helpers/Constants.py
+++ src/helpers/Constants.py
@@ -1,7 +1,7 @@
 from PyQt5.QtGui import QColor
 
 # WARN: must use double quotes around version number (see build-and-release.yaml)
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 
 BACKGROUND = QColor("#4a4e69")
 ACTIVE = QColor("#808080")
